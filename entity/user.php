26b3ba59e6a1f7a14a41f60a2139660bc029e0b1
Vkraminho <touafekakram64@gmail.com>
2025-06-30 14:40:48 +0200

    user

--- entity/user.php
+++ entity/user.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 class User {
     private int $id = 12;
     private string $pseudo = 'toto';
@@ -6,63 +6,55 @@ class User {
     private string $password = '123456';
     private DateTimeImmutable $created_at;
 
-    public function getId() : int{
+    public function __construct($id, $pseudo, $email, $password)
+    {
+        $this->id = $id;
+        $this->pseudo = $pseudo;
+        $this->email = $email;
+        $this->password = $password;
+    }
+
+    public function getId() : int {
         return $this->id;
     }
-    public function getPseudo() : string{
+
+    public function getPseudo() : string {
         return $this->pseudo;
     }
-    public function getEmail(): string{
+
+    public function getEmail() : string {
         return $this->email;
     }
-    public function getPassword() : string{
+
+    public function getPassword() : string {
         return $this->password;
     }
+
     public function getCreated_at() : DateTimeImmutable {
         return $this->created_at;
     }
-    public function setId(int $id) : void{
-    $this->id =$id;
-}
-    public function setPseudo(string $pseudo) : void{
-    $this->pseudo =$pseudo;
-}
-    public function setEmail(string $email) : void{
-    $this->email =$email;
-}
-    public function setPassword(string $password) : void{
-    $this->password =$password;
-}
+
+    public function setId(int $id) : void {
+        $this->id = $id;
+    }
+
+    public function setPseudo(string $pseudo) : void {
+        $this->pseudo = $pseudo;
+    }
+
+    public function setPassword(string $password) : void {
+        $this->password = $password;
+    }
+
+    public function setEmail(string $email) : void {
+        $this->email = $email;
+    }
 }
 
-$user =new User();
-$user2 = new User();
+$user = new User(23, 'korben', 'korben@gmail.com', '123456789');
 
-echo $user->getId() . '<br>';
-echo $user2->getId() . '<br>';
 echo $user->getPseudo() . '<br>';
-echo $user2->getPseudo() . '<br>';
-echo $user->getEmail() . '<br>';
-echo $user2->getEmail() . '<br>';
-echo $user->getPassword() . '<br>';
-echo $user2->getPassword() . '<br>';
 
-$user->setId(25);
-$user->setPseudo('tata');
-$user->setEmail('tata@gmail.com');
-$user->setPassword(654321);
+$user->setPseudo('paul');
 
-
-echo $user->getId() . '<br>';
-echo $user2->getId() . '<br>';
 echo $user->getPseudo() . '<br>';
-echo $user2->getPseudo() . '<br>';
-echo $user->getEmail() . '<br>';
-echo $user2->getEmail() . '<br>';
-echo $user->getPassword() . '<br>';
-echo $user2->getPassword() . '<br>';
-echo $user->getcreated_at() . '<br>';
-echo $user2->getcreated_at() . '<br>';
-
-
-
